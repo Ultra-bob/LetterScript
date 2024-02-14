74156d254977a3e2e4174a8af90963a895c9d976
Ultrablob <neelgillshah@gmail.com>
2024-02-14 14:59:23 -0500

    idk

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
+var WordToNumber = require( "word-to-number-node" );
+var w2n = new WordToNumber();
+
 const function_regex = /([A-Z]+) ([a-zA-Z ]+)/gm
-const code = "ADD one PLUS one"
+const code = "ADD one PLUS twenty five"
 const [_, fn_name, args_str]: RegExpExecArray = function_regex.exec(code)
-const args = args_str.split(/ [A-Z]+ /gm)
+const args = args_str.split(/ [A-Z]+ /gm).map((arg) => w2n.parse(arg) || arg)
 console.log(`${fn_name}(${args})`)
